962d23b8c0682de66c0804368d0e6590fd2da5ef
MarcelSpringsholz <springsholz.m01@htl-ottakring.ac.at>
2020-05-02 15:14:32 +0200

    1.Push

---
+++ website/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      dark
+    >
+      <v-btn large text to="/">MarcelS.</v-btn>
+      <spacer/>
+      <v-btn small text to="Video">Video</v-btn>
+      <v-btn small text to="Photo">Photo</v-btn>
+      <v-btn small text to="Projekt">Projekts</v-btn>
+
+  
+    </v-app-bar>
+
+    <v-content>
+      <Router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ website/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+     <v-parallax
+    dark
+    src="img/IMG_3510.JPG"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col cols="10">
+        <h1 class="display-1 font-weight-thin mb-4">Marcel Springsholz</h1>
+        
+        <h4 class="subheading">Contact Me!</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+  <v-row align="center" justify="center">
+    <v-col cols="10">
+  <p>Ich bin Marcel Springsholz am 28.06.2001 in Wien geboren. Derzeit besuche ich die HTL-WienWest im 5. von 5 Jahren. Ich habe die Medientechnikwelt gewählt, da es mir schon immer Spaß gemacht hat Fotos und Videos zu machen. In meiner Ausbildung befasse ich mich mit den diversen Adobe Produkten wie Indesign, Photoshop, Premiere Pro und vielen Weiteren. Außerdem befasse ich mich mit Affinity Photo & Designer, Blender, Datenbanken und mehreren Programmiersprachen unter anderem C#, JavaScript, HTML, php, sql etc.</p>
+  </v-col>
+  </v-row>
+   <v-row align="center" justify="center">
+    <v-col cols="10">
+  <p>Ich bin Marcel Springsholz am 28.06.2001 in Wien geboren. Derzeit besuche ich die HTL-WienWest im 5. von 5 Jahren. Ich habe die Medientechnikwelt gewählt, da es mir schon immer Spaß gemacht hat Fotos und Videos zu machen. In meiner Ausbildung befasse ich mich mit den diversen Adobe Produkten wie Indesign, Photoshop, Premiere Pro und vielen Weiteren. Außerdem befasse ich mich mit Affinity Photo & Designer, Blender, Datenbanken und mehreren Programmiersprachen unter anderem C#, JavaScript, HTML, php, sql etc.</p>
+  </v-col>
+</v-row>
+  
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
